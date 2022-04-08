e563396debd4e2e35979f99d7c0e4ceeefe2fd16
bwide <bfwide07@gmail.com>
2022-04-08 17:35:16 -0300

    updated tests

--- Tests/LegacyWebViewTests/LegacyWebViewTests.swift
+++ Tests/LegacyWebViewTests/LegacyWebViewTests.swift
@@ -6,6 +6,5 @@ final class LegacyWebViewTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(LegacyWebView().text, "Hello, World!")
     }
 }
